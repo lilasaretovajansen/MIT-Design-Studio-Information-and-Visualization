3e8aae62c2dc81a56b2cc096b6086ed5130cf44e
lilajansen <ljansen@mit.edu>
2018-04-06 14:17:36 -0400

    changes

--- InClass/Week6/script_1.js
+++ InClass/Week6/script_1.js
@@ -11,7 +11,7 @@ var plot1 = d3.select('#plot1') // if we select a html id #name, if we select a 
     .attr('height', height + margin.t + margin.b);
 
 // function to draw the map
-
+var path = d3.geoPath();
 // queue data files, parse them and use them
 var queue = d3.queue()
     .defer(d3.csv, "data/data.csv", parseData)
@@ -19,13 +19,34 @@ var queue = d3.queue()
     .await(dataloaded);
 
 function dataloaded (err,data,map){
-
+    console.log(data);
+    console.log(map);
+    var extentData = d3.extent(data,function(d){return d.total});
+    var colorScale = d3.scaleLinear().domain(extentData).range(["#ffc5c0","#ab0405"]);
+    //console.log(colorScale);
     // get max and min values of data
 
     // scale Color for the map
 
     // Bind the data to the SVG and create one path per GeoJSON feature
-
+    plot1.selectAll(".state").data(topojson.feature(map,map.objects.states).features)
+        .enter()
+        .append('path')
+        .attr("d",path)
+        .attr("class","state")
+        .style("fill",function(d){
+            var color = "rgb(255,255,255)";
+            var mapID = +d.id;
+            data.forEach(function(e){
+                if(e.id.toString() ===mapID.toString()){
+                    color = colorScale(e.total);
+                    console.log(color);
+                    console.log("match");
+                }
+            });
+            return color;
+    });
+    
 }
 
 
@@ -35,5 +56,27 @@ function dataloaded (err,data,map){
 
 
 function parseData(d){
+    var id =(d.Id).split("US")[1];
+    return{
+        id: id,
+        state:d.state,
+        total:+d["Total; Estimate; Population 3 years and over enrolled in school"]
+        
+    }
+}
 
+function parsePopulation(d){
+    var id;
+    if (d.id !=""){
+        id = (d.id).split("US")[1];
+    }
+    else{
+        id = d+["Geographic Area"];
+    }
+    //var id =(d.Id).split("US")[1];
+    populationPerState.set(id,{
+        state:d["Geographic Area"],
+        april2010:+d["April 1,2010, Census"],
+        estimate2017:+d["Estimate 2017"]
+    })
 }
